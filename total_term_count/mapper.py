d4e75ed89f135681828b0aeb384d90bc66883d60
Eric Nunn <ejnunn1@msn.com>
2025-03-10 20:54:35 -0400

    Add filtering for UnicodeDecodeErrors and update comment explicitly stating that stop words are NOT to be filtered out at the total_term_count step

--- total_term_count/mapper.py
+++ total_term_count/mapper.py
@@ -7,9 +7,9 @@ import os
 # Get document ID from the input file name
 docid = os.path.splitext(os.path.basename(os.getenv('map_input_file', 'unknown.txt')))[0]
 
-for line in sys.stdin.buffer.read().decode("utf-8", "replace"):
+for line in sys.stdin.buffer.read().decode("utf-8", "replace").splitlines():
     for word in line.strip().split():
         # Keep only alphabetic characters and convert to lowercase
         term = ''.join(c for c in word.lower() if 'a' <= c <= 'z')
-        if term:  # Ensure term is not empty
+        if term: # Ensure term is not empty but do NOT filter out stop words in this step.
             print(f"{docid}\t1")
