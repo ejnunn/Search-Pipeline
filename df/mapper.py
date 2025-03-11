d0f97e8573d84bdfb60935446ababb7d5d198eec
Eric Nunn <ejnunn1@msn.com>
2025-03-10 20:33:54 -0400

    Upgrade df/mapper to python3

--- df/mapper.py
+++ df/mapper.py
@@ -1,15 +1,16 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 """mapper.py"""
 
 import sys
-import string
 import os
 
-docid = os.path.splitext(os.path.basename(os.getenv('map_input_file')))[0]
+# Extract document ID from filename
+docid = os.path.splitext(os.path.basename(os.getenv('map_input_file', 'unknown')))[0]
 
 for line in sys.stdin:
     for word in line.strip().split():
         lowered = word.lower()
-        term  = filter(lambda c: 97 <= ord(c) <= 122, lowered)
-        if len(term) > 0:
-            print '%s\t%s' % (term, docid)
+        term = ''.join(filter(str.isalpha, lowered))  # Keep only alphabetic characters
+        
+        if term:
+            print(f"{term}\t{docid}")
