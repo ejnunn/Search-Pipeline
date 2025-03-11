c5cbfb12099512cb9988bdfcab0c769b236e54ef
Eric Nunn <ejnunn1@msn.com>
2025-03-10 20:35:11 -0400

    Add run_pipeline.sh script to test the full pipeline locally. Only the first two steps are added right now.

---
+++ run_pipeline.sh
@@ -0,0 +1,25 @@
+# Setup
+corpus=$(ls data/state_union/*.txt)  # Expanding the glob pattern
+mkdir -p output # Ensure the output directory exists
+
+# Run the Term Count Job
+echo "Running the Term Count Job"
+for file in $corpus; do
+    export map_input_file="$file"
+    cat "$file" | python3 term_count/mapper.py | sort | python3 term_count/reducer.py
+done > output/term_counts.txt
+
+# Run the Total Term Count Job
+echo "Running the Total Term Count Job"
+for file in $corpus; do
+    export map_input_file="$file"
+    cat "$file" | python3 total_term_count/mapper.py | sort | python3 total_term_count/reducer.py
+done > output/total_term_counts.txt
+
+# Run the TF Job
+
+
+# Run the DF Job
+
+
+# Compute TF-IDF
