4efbb965c8287cc8b73e3cb0cc1e82a844921cf3
Eric Nunn <ejnunn1@msn.com>
2025-03-13 03:19:06 -0400

    Add stemming to initial processing of corpus to improve search accuracy

--- term_count/mapper.py
+++ term_count/mapper.py
@@ -8,6 +8,7 @@ Identical terms from different documents represent separate term counts in this 
 
 import sys
 import os
+from nltk.stem import PorterStemmer
 
 # Add the 'utils' directory to the sys.path to be able to import utils
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'utils')))
@@ -22,13 +23,16 @@ def get_docid():
         return os.path.splitext(os.path.basename(filepath))[0]
     return "unknown_doc"  # Fallback when running locally without Hadoop
 
+ps = PorterStemmer()
+
 docid = get_docid()
 
 for line in sys.stdin.buffer.read().decode("utf-8", "ignore").splitlines():
     for word in line.strip().split():
         lowered = word.lower()
         term = "".join(filter(lambda c: 97 <= ord(c) <= 122, lowered))  # Only lowercase a-z
+        term_stem = ps.stem(term)
 
-        if term and term not in stop_words:  # Filter out stop words
-            print(f"{term}+{docid}\t1")
+        if term_stem and term_stem not in stop_words:  # Filter out stop words
+            print(f"{term_stem}+{docid}\t1")
 
